f8d3cf0c8315e3be7d4dbe21f8474fbab8466fed
Casey Backes <casey@Caseys-MacBook-Pro.local>
2020-02-10 16:18:49 -0700

    contact and about routes started

--- app.py
+++ app.py
@@ -8,7 +8,21 @@ app = Flask(__name__)
 # home page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    context = {"title":"Home"}
+    return render_template('index.html', context=context)
+
+@app.route('/about')
+def about():
+    context = {"title":"About"}
+    return render_template('about.html', context = context)
+
+@app.route('/contact')
+def contact():
+    context = {'title':"Contact"}
+    return render_template('contact.html', context=context)
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, threaded=True, debug=False)
